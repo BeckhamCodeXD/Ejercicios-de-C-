d01ce37af535e20b7f8bc2989edf7ac5ceab24cf
BeckhamCodeXD <167550607+BeckhamCodeXD@users.noreply.github.com>
2024-05-02 23:32:29 -0500

    cambio
    cambio

--- Ejercicios Lab. C#/Primera tarea de Laboratorio C#/Program.cs
+++ Ejercicios Lab. C#/Primera tarea de Laboratorio C#/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello");
+﻿using System;
+
+
+double n1, n2, suma, resta, multiplicacion, division;
+Console.Write("Ingrese el primer numero : ");
+n1 = double.Parse(Console.ReadLine());
+Console.Write("Ingrese el segundo numero : ");
+n2 = double.Parse(Console.ReadLine());
+suma = n1 + n2;
+resta = n1 - n2;
+multiplicacion = n1 * n2;
+Console.WriteLine("La suma es :" + suma);
+Console.WriteLine("La resta es :" + resta);
+Console.WriteLine("La multiplicacion es :" + multiplicacion);
+if (n2 > 0)
+{
+    division = n1 / n2;
+    Console.WriteLine("La division es :" + Math.Round(division, 2));
+}
+else
+{
+    Console.WriteLine("ERROR.No se pude dividir entre Cero.");
+
+}
