065a49c44e0c5cd429259e37427cf853cca9c008
BeckhamCodeXD <167550607+BeckhamCodeXD@users.noreply.github.com>
2024-05-10 19:46:33 -0500

    cambio

--- Ejercicios Lab. C#/ConsoleApp1/ejercicios de C# 2.cs
+++ Ejercicios Lab. C#/ConsoleApp1/ejercicios de C# 2.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Escribe una función recursiva que imprima los números pares del 1 al 100
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PrintEvenNumbers(1);
+    }
+
+    static void PrintEvenNumbers(int n)
+    {
+        if (n > 100) return;
+        Console.WriteLine(n);
+        PrintEvenNumbers(n + 2);
+    }
+}
